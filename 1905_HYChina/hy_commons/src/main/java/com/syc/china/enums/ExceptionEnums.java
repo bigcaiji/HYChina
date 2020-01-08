b29db1df395b1d94e96080fd380c6084e2c8abcc
wmy <wmy@qq.com>
2020-01-08 19:29:51 +0800

    project

--- 1905_HYChina/hy_commons/src/main/java/com/syc/china/enums/ExceptionEnums.java
+++ 1905_HYChina/hy_commons/src/main/java/com/syc/china/enums/ExceptionEnums.java
@@ -13,7 +13,13 @@ import lombok.NoArgsConstructor;
 public enum ExceptionEnums {
     PRICE_CANNOT_BE_NULL(400,"价格不能为空"),
 
-    UNKNOWN_ERROR(111,"未知错误");
+    UNKNOWN_ERROR(111,"未知错误"),
+
+    REGISTER_CODE_IS_ERROR(601,"验证码为空"),
+
+    ACCOUNT_IS_NOT_EXIST(602,"账户不存在"),
+
+    USERNAME_OR_PASSWORD_ERROR(603,"用户名或密码错误");
 
     private int code;
     private String msg;
